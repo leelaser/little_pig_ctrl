e59750d03334f275223411f7b9eb95d72656fa71
mal-a-chillingg <mal_a_chillingg@yahoo.com>
2020-02-07 18:47:22 -0600

    final backup

--- src/can_bus.py
+++ src/can_bus.py
@@ -1,18 +1,50 @@
-from __future__ import print_function
+#!/usr/bin/env python
+
+#from __future__ import print_function
+import math
+from math import pi
+import rospy
+
+from geometry_msgs.msg import Twist
 
 import can
+import struct
 
-def send_one(output):
+class _CanMove(object):
+
+    def __init__(self):
+        rospy.init_node("teleop_can")
+
+        self._can_steer_angle = 0.0
+        self._can_steer_angle_vel = 0.0
+        
+        self.twist_sub = rospy.Subscriber('/cmd_vel', Twist, self.can_cb, queue_size=1)
+
+    def spin(self):
+        while not rospy.is_shutdown():
+            rospy.loginfo("Attempting...")
+            self.send_one(self._can_steer_angle * 8960 / (2 * pi))
+            rospy.sleep(2.0)
+
     
-    bus0 = can.interface.Bus(bustype='socketcan', channel='can0',bitrate=250000)
-    msg = can.Message(arbitration_id=0x20C,data=(output).to_bytes(4, byteorder="little",signed=True) + (0).to_bytes(4, byteorder="little",signed=True),is_extended_id=False)
+    def can_cb(self, data):
+        cmd_data = data
+
+        self._can_steer_angle = data.angular.z
+
+        return self._can_steer_angle
+
+    def send_one(self, output):
     
-    print(output)
-    try:
-        bus0.send(msg)
-        print("Message sent on {}".format(bus0.channel_info))
-    except can.CanError:
-        print("Message NOT sent")
+        bus0 = can.interface.Bus(bustype='socketcan', channel='can0',bitrate=250000)
+        #msg = can.Message(arbitration_id=0x20C,data=(output).to_bytes(4, byteorder="little",signed=True) + (0).to_bytes(4, byteorder="little",signed=True),is_extended_id=False)
+        msg = can.Message(arbitration_id=0x199,data=struct.pack('<ii',output,0),is_extended_id=False)
+    
+        try:
+            bus0.send(msg)
+            rospy.loginfo("Message sent on {}".format(bus0.channel_info))
+        except can.CanError:
+            rospy.loginfo("Message NOT sent")
 
 #end of def send_one
 
@@ -23,22 +55,6 @@ def send_one(output):
 
 #startup()
 
-pos = 0
-#stuffer = 0
-key_in = "\n";
-
-while(key_in != "f"):
-    key_in = input("input a key")
-    if((key_in == "A") or (key_in == "a")):
-        pos = pos + 1000
-    elif((key_in == "D") or (key_in == "d")):
-        pos = pos - 1000
-
-    print(pos)
-    print((pos).to_bytes(4, byteorder="little", signed=True))
-    send_one(pos)
-
-#if __name__ == '__main__':
-#    send_one()
-
-
+if __name__ == '__main__':
+    ctrl = _CanMove()
+    ctrl.spin()
