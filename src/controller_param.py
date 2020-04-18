7dcfc7251ff1b75a85578bcbe80d5ea61e7f2ff7
JosephChan0510 <josephchan12341@gmail.com>
2020-04-18 15:16:57 -0500

    josephfiles

---
+++ src/controller_param.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import rospy
+
+from geometry_msgs.msg import Twist
+from ackermann_msgs.msg import AckermannDrive
+
+
+class _AckermannMove(object):
+
+    def __init__(self):
+        rospy.init_node("teleop_ackermann")
+
+        self._ackermann_speed = 2.0
+        self._ackermann_accel = 2.0
+        self._ackermann_steer_ang = 0.5
+        self._ackermann_steer_ang_vel = 0.5
+
+        self.ackermann_msg = AckermannDrive()
+        self.twist_sub = rospy.Subscriber('/cmd_vel', Twist, self.ackermann_cb, queue_size=1)
+        self.ackermann_pub = rospy.Publisher('/basic_laser_pig/ackermann_cmd', AckermannDrive, queue_size=1)
+
+    def ackermann_cb(self, data):
+        cmd_data = data
+        self._ackermann_speed = cmd_data.linear.x
+        self.ackermann_msg.speed = self._ackermann_speed
+        self.ackermann_msg.acceleration = self._ackermann_accel
+        self.ackermann_msg.steering_angle_velocity = self._ackermann_steer_ang_vel
+        self.ackermann_msg.steering_angle = cmd_data.angular.z
+
+        return self.ackermann_msg
+
+    def spin(self):
+        while not rospy.is_shutdown():
+            rospy.loginfo("Attempting...")
+            self.ackermann_pub.publish(self.ackermann_msg)
+            rospy.sleep(0.1)
+
+
+if __name__ == '__main__':
+    ctrl = _AckermannMove()
+    ctrl.spin()
